543969d7d41bfb0cc22e3fe35c6023ced465d4c7
Sijawusz Pur Rahnama <sija@sija.pl>
2013-04-19 13:58:55 +0200

    Move source to the root dir level.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__ . '/app/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Gizmo\Silex\Provider(), array(
-    'gizmo.path'           => __DIR__,
+    'gizmo.path'           => dirname($_SERVER['SCRIPT_FILENAME']),
     'gizmo.mount_point'    => '/',
 
     'gizmo.options' => array(
@@ -14,5 +14,3 @@ $app->register(new Gizmo\Silex\Provider(), array(
         'default_layout' => 'base.html.twig',
     )
 ));
-
-$app->run();
